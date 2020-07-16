c576918489414fae2f4c5d914731445df9a3b283
yamazaki <51977859+naikasann@users.noreply.github.com>
2020-07-16 22:29:25 +0900

    :sparkles: pinテスト用のプログラムの追加

---
+++ GaitSensor_pintest/Gaitsensor_pintest.ino
@@ -0,0 +1,26 @@
+// for SPI setting
+#define SPI_SPEED 115200
+
+// e-textile sensor 
+#define TOO_PIN 25
+#define HEEL_PIN 26
+
+void setup() {
+    // for terminal
+    Serial.begin(SPI_SPEED);
+
+    // etextile pinMode setting
+    pinMode(TOO_PIN,  INPUT_PULLUP);
+    pinMode(HEEL_PIN, INPUT_PULLUP);
+
+    Serial.println("Shoes pin test");
+}
+
+void loop() {
+    Serial.print("Shoes state | TOO : ");
+    Serial.print(digitalRead(TOO_PIN));
+    Serial.print(", HEEL : ");
+    Serial.println(digitalRead(HEEL_PIN));
+
+    delay(500);
+}
